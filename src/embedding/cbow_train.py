61f22fdcbf5e5f9494a620b21cfad195bc5152d5
Johannes Lötzsch <git@johannesloetzsch.de>
2024-03-08 22:34:47 +0100

    word2vec

--- src/embedding/cbow_train.py
+++ src/embedding/cbow_train.py
@@ -6,10 +6,8 @@ from torchinfo import summary
 from corpus import Corpus
 from embedding.cbow import cbow_trainingdata_for_corpus
 
-c_full = Corpus()
-c = Corpus(c_full.corpus[1_500:10_000])
-print("Full corpus", c_full.__meta__())
-print("Used corpus", c.__meta__())
+c = Corpus()
+print("Corpus:", c.__meta__())
 
 word_count = len(c.words())
 context_size = 2
@@ -23,11 +21,13 @@ class Net(nn.Module):
     def __init__(self, in_features, out_features, p_dropout=0.1, **args):
         super(Net, self).__init__()
         self.dropout = nn.Dropout(p_dropout)
-        self.fc = nn.Linear(in_features, out_features)
+        self.fc_in = nn.Linear(in_features, args["vector_len"]-1)
+        self.fc_out = nn.Linear(args["vector_len"]-1, out_features)
 
     def forward(self, x):
         x = self.dropout(x)
-        x = self.fc(x)
+        x = self.fc_in(x)
+        x = self.fc_out(x)
         output = F.log_softmax(x, dim=1)
         return output
 
@@ -40,15 +40,16 @@ def train(args, model, device, train_loader, optimizer, epoch):
         output = model(data)
         loss = nn.CrossEntropyLoss()(output, target)
         loss.backward()
+        #nn.utils.clip_grad_norm_(model.parameters(), 1)
         optimizer.step()
     print('Train Epoch: {:4d} \tLoss: {:.6f}'.format(epoch, loss.item()))
 
-args = {"epochs": 1000, "batch_size": 32, "p_dropout": 0, "log_interval": 100}
+args = {"vector_len": 100, "epochs": 20, "batch_size": 32, "p_dropout": 0, "log_interval": 100}
 
 device = torch.device("cpu")
 model = Net(2*context_size*word_count, word_count, **args).to(device)
 print(summary(model))
-optimizer = optim.Adadelta(model.parameters())
+optimizer = optim.Adam(model.parameters())
 train_loader = torch.utils.data.DataLoader(list(zip(X,Y)), batch_size=args["batch_size"])
 
 
@@ -57,3 +58,6 @@ for epoch in range(1, 1+args["epochs"]):
 
 [idx2word, word2idx] = c.translators()
 print(' '.join([idx2word[int(x.argmax())] for x in model(X)]))
+
+[weights, biases] = model.fc_out.parameters()
+vectors = [weights[i].tolist() + [biases[i].tolist()] for i in range(word_count)]
